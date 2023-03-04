34b1ef783ad6712f9f99e9a98fc7dc1f3622d80a
devpaniccia <126923939+devpaniccia@users.noreply.github.com>
2023-03-03 20:41:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def configure_rei_reply_webhook():
         "Content-Type": "application/json",
     }
     payload = {
-        "url": "https://reireply-chatgpt.herokuapp.com/webhook/reireply",
+        "url": "https://git.heroku.com/reireply-chatgpt.git",
         "event": "incoming_message"
     }
     response = requests.post(url, headers=headers, data=json.dumps(payload))
